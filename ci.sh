69038cc278e72dd04559d00049a4db12968754f5
Johan Walles <johan.walles@gmail.com>
2018-06-24 22:37:22 +0200

    Clear up Python shebang confusion

--- ci.sh
+++ ci.sh
@@ -7,11 +7,7 @@ set -e
 bazel test --test_output=errors --python_path=/usr/local/bin/python2 unittests-py2
 bazel test --test_output=errors --python_path=/usr/local/bin/python3 unittests-py3
 
-# FIXME: Can this thing be convinced to add a python shebang?
 bazel build --build_python_zip bin
 
-# Add Python shebang so that it looks like a pex file
-echo '#!/usr/bin/env python' | cat - ./bazel-bin/bin.zip > ./bin.pex
-chmod a+x ./bin.pex
-
-./bin.pex
+# This file should have a Python shebang and be a complete zip file
+./bazel-bin/bin
